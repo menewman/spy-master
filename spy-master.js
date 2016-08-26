48d6d0c1589ecd8046866424e20cd632a4de0265
Michael Newman <michaeleugenenewman@gmail.com>
2016-08-26 16:27:10 -0500

    Create spy-master.js

---
+++ spy-master.js
@@ -0,0 +1,32 @@
+SpyMaster = (function() {
+  var defaultTag = 'SpyLogger';
+
+  var spyOnOwn = function(obj, tag) {
+    tag = tag || defaultTag;
+    var prefix = tag + ': ';
+
+    (Object.keys(obj) || [])
+      .filter(function(key) { return typeof obj[key] === 'function'; })
+      .forEach(function(key) {
+        var originalFunc = obj[key];
+        obj[key] = function() {
+          console.log(prefix + key);
+          return originalFunc.apply(this, arguments);
+        };
+      });
+  };
+
+  var spyOnPrototype = function(obj, tag) {
+    var proto = Object.getPrototypeOf(obj);
+    spyOnOwn(proto, tag);
+  };
+
+  var infiltrate = function(obj, tag) {
+    spyOnOwn(obj, tag);
+    spyOnPrototype(obj, tag);
+  };
+
+  return {
+    infiltrate: infiltrate
+  };
+})();
