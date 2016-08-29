f555173b8c4c87ad2efbd9b7fe5c07a0636a966f
Michael Newman <michael.newman@inin.com>
2016-08-29 11:18:05 -0500

    log arguments; allow skipping specified keys

--- spy-master.js
+++ spy-master.js
@@ -1,30 +1,41 @@
 SpyMaster = (function() {
   var defaultTag = 'SpyMaster';
 
-  var spyOnOwn = function(obj, tag) {
+  // obj          - an object / class instance to spy on
+  // tag          - an optional tag to use when logging to the console
+  // keysToIgnore - an object containing keys to ignore, like { foo: true, bar: true }
+  function spyOnOwn(obj, tag, keysToIgnore) {
     tag = tag || defaultTag;
+    keysToIgnore = keysToIgnore || {};
     var prefix = tag + ': ';
 
     (Object.keys(obj) || [])
-      .filter(function(key) { return typeof obj[key] === 'function'; })
+      .filter(function(key) {
+        return !keysToIgnore[key] && typeof obj[key] === 'function';
+      })
       .forEach(function(key) {
         var originalFunc = obj[key];
         obj[key] = function() {
-          console.log(prefix + key);
-          return originalFunc.apply(this, arguments);
+          var args = Array.prototype.slice.call(arguments);
+
+          console.log(prefix + key + " / " + args.length + " arguments");
+          if (args.length) {
+            console.dir(args);
+          }
+          return originalFunc.apply(this, args);
         };
       });
-  };
+  }
 
-  var spyOnPrototype = function(obj, tag) {
+  function spyOnPrototype(obj, tag, keysToIgnore) {
     var proto = Object.getPrototypeOf(obj);
-    spyOnOwn(proto, tag);
-  };
+    spyOnOwn(proto, tag, keysToIgnore);
+  }
 
-  var infiltrate = function(obj, tag) {
-    spyOnOwn(obj, tag);
-    spyOnPrototype(obj, tag);
-  };
+  function infiltrate(obj, tag, keysToIgnore) {
+    spyOnOwn(obj, tag, keysToIgnore);
+    spyOnPrototype(obj, tag, keysToIgnore);
+  }
 
   return {
     infiltrate: infiltrate
